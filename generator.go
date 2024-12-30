25ff16416c403272edb3ef5f74cf6af269d3d8e8
4strodev <jumarurex@gmail.com>
2024-12-30 21:51:54 +0100

    fix:return emoji directly

--- generator.go
+++ generator.go
@@ -1,40 +1,9 @@
 package emoji
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-	"unicode/utf8"
-)
-
-func getEmoji(emoji string) string {
-	code, exists := emojiMap[emoji]
+func getEmoji(name string) string {
+	emoji, exists := emojiMap[name]
 	if !exists {
 		return ""
 	}
-	res := ""
-	chars := utf8.RuneCountInString(code)
-	curChar := 1
-	for _, c := range code {
-		tmp := strings.Trim(strings.ToLower(strconv.QuoteRuneToASCII(c)), "'")
-		if tmp != `\ufe0f` || (chars > 2 && curChar == chars) {
-			// Valid character to add
-			if curChar > 1 {
-				res += "-"
-			}
-			if len(tmp) == 1 {
-				res += fmt.Sprintf("%x", []byte(tmp))
-			} else {
-				res += strings.TrimLeft(tmp[2:], "0")
-			}
-		}
-		curChar++
-	}
-
-	return res
-}
-
-func generateURL(emoji string) string {
-	res := getEmoji(emoji)
-	return fmt.Sprintf("https://twemoji.maxcdn.com/2/svg/%s.svg", res)
+        return emoji
 }
